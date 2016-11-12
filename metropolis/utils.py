b1f3df5232104a8f2367f2d698f3186a93c4b933
Abhinav <abhinav.venigalla@gmail.com>
2016-11-12 01:45:03 -0500

    added boxes

---
+++ metropolis/utils.py
@@ -0,0 +1,84 @@
+from PIL import Image,ImageDraw
+import numpy as np
+
+def get_box(size, loc):
+  sides = [
+  [[loc[i]+dr[i] for i in range (0,3)] for dr in [[0,0,0],[size,0,0],[size,size,0],[0,size,0]]],
+  [[loc[i]+dr[i] for i in range (0,3)] for dr in [[0,0,0],[size,0,0],[size,0,size],[0,0,size]]],
+  [[loc[i]+dr[i] for i in range (0,3)] for dr in [[0,0,0],[0,0,size],[0,size,size],[0,size,0]]],
+  [[loc[i]+dr[i] for i in range (0,3)] for dr in [[0,size,0],[size,size,0],[size,size,size],[0,size,size]]],
+  [[loc[i]+dr[i] for i in range (0,3)] for dr in [[size,0,0],[size,size,0],[size,size,size],[size,0,size]]],
+  [[loc[i]+dr[i] for i in range (0,3)] for dr in [[0,0,size],[0,size,size],[size,size,size],[size,0,size]]]
+  ]
+
+  return [np.array(side) for side in sides]
+  
+
+def draw_polygons(drawer, polygons):
+  w = drawer.im.size[0]
+  h = drawer.im.size[1]
+  for t in polygons:
+    if t[2]:
+      drawer.polygon(map(lambda p: (p[0]+w/2,h/2-p[1]),t[0]),outline=t[1])
+    else:
+      drawer.polygon(map(lambda p: (p[0]+w/2,h/2-p[1]),t[0]),fill=t[1])
+
+def local_coord(M, points):
+  inv = np.linalg.inv(M)
+  aug = np.concatenate((points, np.ones((len(points),1))), axis=1)
+  return np.dot(aug,inv)[:,:-1]
+
+def project(local_coord, fov):
+  return np.apply_along_axis(lambda r: fov*r/-r[-1], 1, local_coord)[:,:-1]
+
+def get_local_proj(M,points,fov):
+  local = local_coord(M, points)
+  return project(local,fov)
+
+def draw_from_file(drawer, filename, fov):
+  f = open(filename).readlines()
+  camera = np.matrix([map(int, t[1:-1].split(",")) for t in f[0].strip().split(" ")])
+  M = np.concatenate((camera, np.array([[0,0,0,1]]).T), axis=1)
+
+  polygons = []
+  for line in f[1:]:
+    arr = line.strip().split(" ")
+
+    if arr[0] == "box":
+      size = int(arr[1])
+      loc = map(int,arr[2][1:-1].split(","))
+      color = arr[3]
+      wireframe = int(arr[4])
+
+      sides = get_box(size,loc)
+      for side in sides:
+        proj = get_local_proj(M,side,fov)
+        polygons.append([map(tuple, np.array(proj)),color,wireframe])
+
+    else:
+      points = np.array([map(int, t[1:-1].split(",")) for t in arr[0:3]])
+      color = arr[3]
+      wireframe = int(arr[4])
+
+      proj = get_local_proj(M,points,fov)
+      polygons.append([map(tuple, np.array(proj)),color,wireframe])
+
+  
+
+
+  draw_polygons(drawer, polygons)
+
+
+
+#MAIN
+#######################################33
+mode = "RGB"
+size = (512,512)
+color = "#ffffff"
+fov = 200
+
+im = Image.new(mode,size,color)
+draw = ImageDraw.Draw(im)
+
+draw_from_file(draw,"polygons.txt", fov)
+im.show()
