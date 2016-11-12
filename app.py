7ebde9a57da67cf4bcba049b60d76d2489c2ea19
Anthony Liu <anthonyscottliu@gmail.com>
2016-11-12 16:55:25 -0500

    Makes the metropolis code a module and uses it in the webapp.

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, redirect, request, jsonify
 from PIL import Image
 from io import BytesIO
+import numpy as np
 import base64
+from metropolis.metropolis import CubeProblem
+from metropolis.mh import MH
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
     return redirect('http://github.com/turbomaze/metropolis', 301)
+
 
 @app.route('/infer', methods=['GET', 'POST'])
 def infer():
@@ -16,84 +20,23 @@ def infer():
     else:
         base64_img = request.form['img']
         img = Image.open(BytesIO(base64.b64decode(base64_img)))
-        obj = [
-            {
-               "shape":"cube",
-               "x":10,
-               "y":0,
-               "z":0,
-               "l":1,
-               "h":20,
-               "w":20,
-               "xRot":0,
-               "yRot":0,
-               "zRot":0,
-               "r":1.0,
-               "g":0.0,
-               "b":0.0
-            },
-            {
-               "shape":"cube",
-               "x":0,
-               "y":0,
-               "z":10,
-               "l":20,
-               "h":20,
-               "w":1,
-               "xRot":0,
-               "yRot":0,
-               "zRot":0,
-               "r":0.0,
-               "g":0.0,
-               "b":1.0
-            },
-            {
-               "shape":"cube",
-               "x":0,
-               "y":0,
-               "z":-10,
-               "l":20,
-               "h":20,
-               "w":1,
-               "xRot":0,
-               "yRot":0,
-               "zRot":0,
-               "r":0.0,
-               "g":0.0,
-               "b":0.0
-            },
-            {
-               "shape":"cube",
-               "x":4,
-               "y":5,
-               "z":3,
-               "l":1.2,
-               "h":3,
-               "w":2,
-               "xRot":70,
-               "yRot":20,
-               "zRot":10,
-               "r":0.7,
-               "g":0.2,
-               "b":0.9
-            },
-            {
-               "shape":"cube",
-               "x":-5,
-               "y":4,
-               "z":-2,
-               "l":3,
-               "h":0.8,
-               "w":1.2,
-               "xRot":30,
-               "yRot":50,
-               "zRot":0,
-               "r":0.9,
-               "g":0.0,
-               "b":0.3
-            }
-        ]
-        return jsonify(obj)
+        r, g, b, a = img.split()
+        problem = CubeProblem(
+            None, (400, 300),
+            mins=[0, 0, 5], maxes=[20, 20, 12],
+            radius=20
+        )
+        black = MH(
+            problem.get_next,
+            problem.get_likelihood_func,
+            problem.get_prior_prob,
+            lambda x: x
+        )
+        first_guess = problem.get_random_cube()
+        guess = black.optimize(
+            img, first_guess, trials=200
+        )
+        return jsonify(params=guess)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
